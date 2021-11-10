2db0744c7dd8b7f1c3d6013276f0b6e401128a03
pinglau_lp@163.com <root@puppet.globomantics.loc>
2021-11-10 02:13:48 -0500

    resource ordering relationship of "before" and "notify"

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,14 +4,20 @@ class pasture {
   package {'pasture':
     ensure  => present,
     provider=> 'gem',
+    before  => [File['/etc/pasture_config.yaml'],
+                File['/etc/systemd/system/pasture.service']],
   }
 
-
+###before: specify its influencing relationship;  use Array when having multiple parameter within same resource type
+###notify: specify its influencing relationship + refresh its influenced resource
+  
   file {'/etc/pasture_config.yaml':
     source  => 'puppet:///modules/pasture/pasture_config.yaml',
+    notify  => Service['pasture'],
   }
   file {'/etc/systemd/system/pasture.service':
     source  => 'puppet:///modules/pasture/pasture.service',
+    before  => Service['pasture'],
   }
 
   service {'pasture':
