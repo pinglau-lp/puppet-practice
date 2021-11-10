4f74dc8bfd0a1949fbf4cb9242b6c9522ef45e47
pinglau_lp@163.com <root@puppet.globomantics.loc>
2021-11-10 02:25:53 -0500

    resource ordering relationship of "require" and "subscribe"

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,19 +8,22 @@ class pasture {
                 File['/etc/systemd/system/pasture.service']],
   }
 
-###before: specify its influencing relationship;  use Array when having multiple parameter within same resource type
-###notify: specify its influencing relationship + refresh its influenced resource
+###require: specify its influenced relationship;  use Array when having multiple parameter within same resource type
+###subscribe: specify its influenced relationship + refresh itself
   
   file {'/etc/pasture_config.yaml':
     source  => 'puppet:///modules/pasture/pasture_config.yaml',
-    notify  => Service['pasture'],
   }
   file {'/etc/systemd/system/pasture.service':
     source  => 'puppet:///modules/pasture/pasture.service',
-    before  => Service['pasture'],
   }
 
   service {'pasture':
     ensure  => 'running',
+    subscribe=> File['/etc/pasture_config.yaml'],
+    require => File['/etc/systemd/system/pasture.service'],
   }
+###PS.被影响的资源 Service['pasture']，把对两份文件的被影响关系都写到自己这里会显得concise
+###同理Package['pasture']，把before的两个关系写在它那 好于 分别在File['/etc/pasture_config.yaml']跟 File['/etc/systemd/system/pasture.service']里写对它的require
+
 }
